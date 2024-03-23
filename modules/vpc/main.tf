e352644fad1e4ee9ff2dfce8f9bf6e2b7265522c
Amin Bista <aminbista6666@gmail.com>
2024-03-23 13:03:54 +0545

    feat: manually specified az for subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -32,6 +32,7 @@ resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.demo_vpc.id
   cidr_block = var.private_subnet_cidr
   map_public_ip_on_launch = true
+  # availability_zone = "ap-southeast-1a"
 
   tags = {
     Name = "demo-private-subnet"
